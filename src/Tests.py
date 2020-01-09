ca6aac752885c01e106d9bf67f4ceeb8d47b409b
texast99 <travis.bonneau@gmail.com>
2020-01-08 23:02:37 -0600

    Sort the words in order of how they should be read

--- src/Tests.py
+++ src/Tests.py
@@ -1,9 +1,17 @@
 import cv2 as cv
 import numpy as np
+from src.Word import Word
+from operator import itemgetter
 
 FILE_NAME = "../images/page.jpg"
 RESIZE_RATIO = 0.65
 
+
+def getWordOrder(words, start, stop):
+    print(start, stop)
+    wordsInRange = list(filter(lambda w: start <= w.getCenter()[1] <= stop, words))
+    sortedWords = sorted(wordsInRange, key=lambda w: w.getCenter()[0])
+    return sortedWords
 
 # Tracks over all words
 def onTrack(data):
@@ -11,31 +19,29 @@ def onTrack(data):
     temp = img.copy()
 
     # Draw center point of word
-    M = cv.moments(cont[data])
-    cx = int(M['m10'] / (M['m00'] + 1E-5))
-    cy = int(M['m01'] / (M['m00'] + 1E-5))
-    cv.circle(temp, (cx, cy), 2, (0, 0, 255), 2)
+    word = words[data]
+    cv.circle(temp, word.getCenter(), 2, (0, 0, 255), 2)
 
     # Draw bounding box around word, based on far left and right
     # and the top most and bottom most pixel
-    x, y, w, h = cv.boundingRect(cont[data])
+    x, y, w, h = cv.boundingRect(word.getContour())
     cv.rectangle(temp, (x, y), (x + w, y + h), (0, 0, 255), 1)
     cv.imshow("Image", temp)
-    cv.imwrite("one_words.png", temp)
 
 
 cv.namedWindow("Image")
 # Read in image and resize to fit screen
 img = cv.imread(FILE_NAME, cv.IMREAD_COLOR)
 img = cv.resize(img, (0, 0), fx=RESIZE_RATIO, fy=RESIZE_RATIO)
+img2 = img.copy()
 
-# Use Canny to get edges of characters
+# Use Adaptive Thresholding to get individual characters
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 gray = cv.bilateralFilter(gray, 11, 75, 75)
-edge = cv.Canny(gray, 50, 150)
+edge = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 11, 12)
 
 # Show image pre any morphological methods
-cv.imshow("Pre-Edge", edge)
+# cv.imshow("Pre-Edge", edge)
 
 # Dilate to connect letters
 kernel = np.ones((7, 7), np.uint8)
@@ -59,19 +65,47 @@ cv.imshow("Image", img)
 cv.createTrackbar("Word", "Image", 0, len(cont)-1, onTrack)
 
 # Draw the bounding boxes for all the words, and the center points
-img2 = img.copy()
+words = []
 for i in range(0, len(cont)):
-    M = cv.moments(cont[i])
-    cx = int(M['m10'] / (M['m00'] + 1E-5))
-    cy = int(M['m01'] / (M['m00'] + 1E-5))
-    cv.circle(img2, (cx, cy), 2, (0, 0, 255), 2)
-
+    words.append(Word(cont[i]))
     x, y, w, h = cv.boundingRect(cont[i])
     cv.rectangle(img2, (x, y), (x + w, y + h), (0, 0, 255), 1)
 
+# whiteCounts helps separate the sentences
+whiteCount = [0] * gray.shape[0]
+for r in range(0, gray.shape[0]):
+    count = 0
+    for c in range(0, gray.shape[1]):
+        if edge[r, c] != 0:
+            count += 1
+    whiteCount[r] = count
+
+# Sort words by sentences
+orderedWords = []
+i = 0
+while i < len(whiteCount):
+    if whiteCount[i] != 0:
+        startIndex = i
+        while i < len(whiteCount) and whiteCount[i] != 0:
+            i += 1
+        orderedWords.extend(getWordOrder(words, startIndex, i))
+    else:
+        i += 1
+words = orderedWords
+
+# Draw lines to show order of words
+prev = None
+for word in words:
+    if prev is not None:
+        cv.line(img2, prev.getCenter(), word.getCenter(), (255, 255, 0), 2)
+    prev = word
+
+# Draw center points of words
+for word in words:
+    cv.circle(img2, word.getCenter(), 2, (0, 0, 255), 2)
+
 onTrack(0)
 cv.imshow("Words", img2)
-cv.imwrite("all_words.png", img2)
-cv.imshow("Edge", edge)
+# cv.imshow("Edge", edge)
 cv.waitKey(0)
 cv.destroyAllWindows()
---
+++ src/Word.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+class Word:
+    def __init__(self, contour):
+        self.contour = contour
+        self.moment = cv.moments(contour)
+
+    def getContour(self):
+        return self.contour
+
+    def getMoment(self):
+        return self.moment
+
+    def getCenter(self):
+        cx = int(self.moment['m10'] / (self.moment['m00'] + 1E-5))
+        cy = int(self.moment['m01'] / (self.moment['m00'] + 1E-5))
+        return cx, cy
+
+    def __str__(self):
+
+        return "(" + self.getCenter()[0] + ", " + self.getCenter()[1] + ")"
